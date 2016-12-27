bffe5ce3d8bccca35fb071ed445bfed7588d2d18
Rajiv Ratan Reddy Dwarampudi <Rajiv Ratan Reddy Dwarampudi>
2016-12-27 11:31:59 -0500

    finalise tests and add page title

--- app/tests/components/countdown.test.jsx
+++ app/tests/components/countdown.test.jsx
@@ -24,5 +24,40 @@ describe('Countdown', () => {
                 done();
             }, 1001);
         });
+
+        it('should never set count less than zero', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(1);
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                done();
+            }, 3001);
+        });
+
+        it('should pause countdown on pause status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('paused');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(3);
+                expect(countdown.state.countdownStatus).toBe('paused');
+                done();
+            }, 1001);
+        });
+
+        it('should pause countdown on pause status', (done) => {
+            var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+            countdown.handleSetCountdown(3);
+            countdown.handleStatusChange('stopped');
+
+            setTimeout(() => {
+                expect(countdown.state.count).toBe(0);
+                expect(countdown.state.countdownStatus).toBe('stopped');
+                done();
+            }, 1001);
+        });
+        
     }); 
 });
